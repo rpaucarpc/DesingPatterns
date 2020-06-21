66cfef6cb708f60d2faebd091cf8766622e7e13c
Rolando <rpaucarpc@gmail.com>
2020-06-20 19:13:17 -0500

    Add project files.

---
+++ SingletonPatternEx1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SingletonPatternEx1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("******* Singleton Pattern Demo *******");
+            Console.WriteLine("Trying to create instance s1.");
+            Singleton s1 = Singleton.Instance;
+
+            Console.WriteLine("Trying to create instance s2");
+            Singleton s2 = Singleton.Instance;
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Only one instance exists.");
+            }
+            else
+            {
+                Console.WriteLine("Different instances exist.");
+            }
+            
+        }
+    }
+}
---
+++ SingletonPatternEx1/Singleton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonPatternEx1
+{
+    public sealed class Singleton
+    {
+        private static readonly Singleton instance = new Singleton();
+        private int numberOfInstances = 0;
+
+        private Singleton() 
+        {
+            Console.WriteLine("Instantiating inside the private constructor.");
+            numberOfInstances++;
+            Console.WriteLine("Number of instances = {0}", numberOfInstances);
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                Console.WriteLine("We already have an instance now. Use it");
+                return instance;
+            }
+        }
+    }
+}
