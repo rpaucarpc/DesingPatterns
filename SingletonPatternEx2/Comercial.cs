6a2ddb5b8d43dda8529e9107acd58bf919a2f005
Rolando <rpaucarpc@gmail.com>
2020-06-20 20:11:33 -0500

    Segundo ejemplo de patron Singleton

---
+++ SingletonPatternEx2/Comercial.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace SingletonPatternEx2
+{
+    public sealed class Comercial
+    {
+        private static readonly Comercial instance = new Comercial();
+
+        public string Nombre { get; set; }
+        public string Direccion{ get; set; }
+        public string Email{ get; set; }
+
+        private Comercial()
+        {
+
+        }
+
+        public static Comercial Instance 
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        public void Visualiza()
+        {
+            Console.WriteLine("Nombre: {0}", Nombre);
+            Console.WriteLine("Dirección: {0}", Direccion);
+            Console.WriteLine("Email: {0}", Email);
+        }
+
+    }
+}
---
+++ SingletonPatternEx2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SingletonPatternEx2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Comercial miComercial = Comercial.Instance;
+            miComercial.Nombre = "Comercial Auto";
+            miComercial.Direccion = "Lima";
+            miComercial.Email = "comercial@gmail.com";
+
+            miComercial.Visualiza();
+
+            Console.WriteLine("*****************");
+
+            Comercial miComercial2 = Comercial.Instance;
+            miComercial2.Visualiza();
+
+
+        }
+    }
+}
